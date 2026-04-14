f86681eaec1ec5d2f4b97182c228d32f10b40dc8
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-14 19:45:47 +0000

    docs: add docstrings to ClauseOperators.cs

--- XivApiSharp.Client/Core/Clauses/Enums/ClauseOperators.cs
+++ XivApiSharp.Client/Core/Clauses/Enums/ClauseOperators.cs
@@ -3,13 +3,36 @@ namespace XivApiSharp.Client.Core.Clauses.Enums;
 /// <summary>
 /// Represents the operation that the clause will use to compare its value to.
 /// </summary>
-/// <seealso cref="Clause{T}"/>
+/// <seealso cref="Clause{T}">Clause&lt;T&gt;</seealso>
 public enum ClauseOperators
 {
+    /// <summary>
+    /// Uses the <c>"~="</c> operator.
+    /// </summary>
     PartiallyEqualTo,
+    
+    /// <summary>
+    /// Uses the <c>=</c> operator.
+    /// </summary>
     EqualTo,
+    
+    /// <summary>
+    /// Uses the <c>&lt;</c> operator.
+    /// </summary>
     LessThan,
+    
+    /// <summary>
+    /// Uses the <c>&lt;=</c> operator.
+    /// </summary>
     LessThanOrEqualTo,
+    
+    /// <summary>
+    /// Uses the <c>&gt;</c> operator.
+    /// </summary>
     GreaterThan,
+    
+    /// <summary>
+    /// Uses the <c>&gt;=</c> operator.
+    /// </summary>
     GreaterThanOrEqualTo
 }

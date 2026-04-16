a00283895767cfcb93f3bce3071332aba772fa55
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-16 18:48:25 +0000

    chore: make IClause public

--- XivApiSharp.Client/Core/Clauses/IClause.cs
+++ XivApiSharp.Client/Core/Clauses/IClause.cs
@@ -8,7 +8,7 @@ namespace XivApiSharp.Client.Core.Clauses;
 /// Implementations of <see cref="IClause"/> are expected to provide a strongly-typed <c>Value</c>
 /// property, exposing it as <see cref="object"/> for use in generic contexts.
 /// </remarks>
-internal interface IClause
+public interface IClause
 { 
     /// <summary>
     /// Name of the field to compare the value against.

91af1a1cf8d23c874c396d635654ee5f49c7c6b9
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-18 23:13:54 +0000

    docs: add docstrings to StringExtensions

--- XivApiSharp.Client/Core/Extensions/StringExtensions.cs
+++ XivApiSharp.Client/Core/Extensions/StringExtensions.cs
@@ -1,9 +1,18 @@
 namespace XivApiSharp.Client.Core.Extensions;
 
+/// <summary>
+/// Provides additional functionality to strings.
+/// </summary>
 public static class StringExtensions
 {
     extension(string input)
     {
+        /// <summary>
+        /// Capitalizes the first character of the string.
+        /// </summary>
+        /// <returns>
+        /// The resulting string.
+        /// </returns>
         public string ToFirstCapital() =>
             $"{input[0].ToString().ToUpper()}{input[1..]}";
     }

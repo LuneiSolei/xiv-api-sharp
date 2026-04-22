69535715b840ed268061c9f15093a7146d9ec8f2
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-22 22:38:34 +0000

    chore: add string conversion methods to IClauseGroup.cs

--- XivApiSharp.Client/Core/ClauseGroups/IClauseGroup.cs
+++ XivApiSharp.Client/Core/ClauseGroups/IClauseGroup.cs
@@ -1,7 +1,28 @@
 namespace XivApiSharp.Client.Core.ClauseGroups;
  
-// TODO: Implement
-public interface IClauseGroup
+/// <summary>
+/// Represents a type that is used to define a group of clauses. This clause 
+/// group is then utilized by <see cref="QueryString"/>.
+/// </summary>
+internal interface IClauseGroup
 {
+    /// <summary>
+    /// Acts as a wrapper for <see cref="ToEncodedString"/>. 
+    /// </summary>
+    /// <returns>A URI encoded string representing this instance.</returns>
     string ToString();
+
+    /// <summary>
+    /// Converts this instance into its URI encoded string representation.
+    /// </summary>
+    /// <returns>A URI encoded string representing this instance.</returns>
+    /// <seealso cref="ToUnencodedString"/>
+    string ToEncodedString();
+
+    /// <summary>
+    /// Converts this instance into its unencoded string representation.
+    /// </summary>
+    /// <returns>A string representing this instance.</returns>
+    /// <seealso cref="ToEncodedString"/>
+    string ToUnencodedString();
 }

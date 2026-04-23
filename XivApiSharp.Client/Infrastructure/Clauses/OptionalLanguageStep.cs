b849af883818fc359702282f649d3ce15645cd3a
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-23 22:13:29 +0000

    chore: convert primary into explicit constructor

--- XivApiSharp.Client/Infrastructure/Clauses/OptionalLanguageStep.cs
+++ XivApiSharp.Client/Infrastructure/Clauses/OptionalLanguageStep.cs
@@ -4,10 +4,21 @@ using XivApiSharp.Client.Core.Clauses;
 namespace XivApiSharp.Client.Infrastructure.Clauses;
 
 /// <inheritdoc cref="IOptionalLanguageStep"/>
-internal sealed class OptionalLanguageStep(IClauseFactory factory, string specifier) 
-    : OptionalDecoratorStep(factory, specifier, SchemaLanguage.None), 
-    IOptionalLanguageStep
+internal sealed class OptionalLanguageStep
+    : OptionalDecoratorStep, IOptionalLanguageStep
 {
+    /// <summary>
+    /// Constructs a new instance of <see cref="OptionalLanguageStep"/>.
+    /// </summary>
+    /// <param name="factory">
+    /// The clause factory used to create new clauses.
+    /// </param>
+    /// <param name="specifier">
+    /// The name of the field specifier for the clause to use.
+    /// </param>
+    public OptionalLanguageStep(IClauseFactory factory, string specifier) 
+        : base(factory, specifier, SchemaLanguage.None) { }
+    
     /// <inheritdoc/>
     public IOptionalDecoratorStep WithLanguage(SchemaLanguage lang)
     {
